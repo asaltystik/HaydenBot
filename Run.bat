d2bfbf514494e9c169e264b6218a4e52bd58c80b
asaltystik <44656529+asaltystik@users.noreply.github.com>
2023-03-06 18:09:51 -0500

    Run Batch File to install and run the bot

---
+++ Run.bat
@@ -0,0 +1,51 @@
+@echo off
+
+rem Change to the directory where this batch file is located
+cd %~dp0
+
+rem Set the name of the Python script to run
+set SCRIPT_NAME=main.py
+
+rem Find the root folder of the Python script
+set ROOT_FOLDER=%~dp0
+
+rem update pip
+pip3 install --upgrade pip
+
+rem Check if pip installed pandas
+pip3 show pandas > nul 2>&1
+if errorlevel 1 (
+    echo "Pandas is not installed. Installing..."
+    pip3 install pandas
+)
+
+rem check if pip installed Selenium
+pip3 show selenium > nul 2>&1
+if errorlevel 1 (
+    echo "Selenium is not installed. Installing..."
+    pip3 install selenium
+)
+
+rem check if pip installed BeautifulSoup4
+pip3 show bs4 > nul 2>&1
+if errorlevel 1 (
+    echo "BeautifulSoup4 is not installed. Installing..."
+    pip3 install bs4
+)
+
+rem check if pip installed chromedriver
+pip3 show chromedriver > nul 2>&1
+if errorlevel 1 (
+    echo "Chromedriver is not installed. Installing..."
+    pip3 install chromedriver
+)
+
+rem check if pip installed requests
+pip3 show requests > nul 2>&1
+if errorlevel 1 (
+    echo "Requests is not installed. Installing..."
+    pip3 install requests
+)
+
+rem Run the Python script
+python3 "%ROOT_FOLDER%%SCRIPT_NAME%"
