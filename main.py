a01c465e5ab2ccb4cbe7f6f7dad308021f8f3ada
asaltystik <44656529+asaltystik@users.noreply.github.com>
2023-03-13 13:50:25 -0400

    Better Menu

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import DuvalCounty
+import ClayCounty
 
 
 # call main function
 if __name__ == "__main__":
-    DuvalCounty.Menu()
+    active = True
+    while active:
+        print("Property Search Bot")
+        print("Select a county to search")
+        print("1. Duval County")
+        print("2. Clay County")
+        print("3. Exit")
+        # get user input
+        county = input("Enter a number: ")
+        # check user input
+        if county == "1":
+            DuvalCounty.Menu()
+        elif county == "2":
+            ClayCounty.Menu()
+        elif county == "3":
+            active = False
+            exit()
+        else:
+            print("Invalid input")
 
